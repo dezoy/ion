8e3eb6e3f31598d0f86fa595674bc1c37a46315e
Anton <litto82@gmail.com>
2019-12-12 16:03:30 +0300

    m

--- sdk/js/src/index.js
+++ sdk/js/src/index.js
@@ -2,7 +2,7 @@
 import Client from './Client'
 import Stream from './Stream'
 
-window.Client = Client
-window.Stream = Stream
+window.Client = Client;
+window.Stream = Stream;
 
 export { Client, Stream }
