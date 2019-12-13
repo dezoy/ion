3156fc345ae003bcd4a6eeab22bff2265cf0f5c9
Anton <litto82@gmail.com>
2019-12-13 12:56:42 +0300

    back demo

--- scripts/centos/webStart.sh
+++ scripts/centos/webStart.sh
@@ -3,8 +3,8 @@
 APP_DIR=$(cd `dirname $0`/../../; pwd)
 cd $APP_DIR/sdk/js
 npm i
-# cd $APP_DIR/sdk/js/demo
-# npm i
+cd $APP_DIR/sdk/js/demo
+npm i
 
 
 nohup npm start 2>&1& echo $! > $APP_DIR/configs/node.pid
